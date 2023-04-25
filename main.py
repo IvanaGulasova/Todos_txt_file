1e6fba791ff814b5f0d6d6353bae9530142a9536
Ivana Gulasova <ivana.gulasova3@gmail.com>
2023-04-25 15:38:31 +0200

    adding functions file

--- main.py
+++ main.py
@@ -1,16 +1,4 @@
-def get_todos(filepath="todos.txt"):
-    """ Read a text file and return the list of
-    to-do items.
-    """
-    with open(filepath, "r") as file_local:
-        todos_local = file_local.readlines()
-    return todos_local
-
-def write_todos(todos_arg ,filepath="todos.txt"):
-    """ Write the to-do items list in the text line."""
-    with open(filepath, "w", encoding="utf-8") as file:
-        file.writelines(todos_arg)
-
+from functions import get_todos, write_todos
 
 while True:
     choice = input("Type add, show, edit, complete or exit: ")
